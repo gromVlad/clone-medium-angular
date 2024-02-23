31904e443f850285719fa09e6ec7bb9cbe3d6390
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2024-02-23 14:04:23 +0300

    feat: add scss feed global, your-feed ...

--- src/app/shared/components/tag-list/tag-list.component.ts
+++ src/app/shared/components/tag-list/tag-list.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tag-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './tag-list.component.html',
   styleUrl: './tag-list.component.scss',
 })
