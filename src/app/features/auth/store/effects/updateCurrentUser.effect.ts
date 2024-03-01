e8457b9e21bf9353734ca575422f59f5beb7339f
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2024-03-01 14:53:00 +0300

    fix: without updateUser(put / 500)

--- src/app/features/auth/store/effects/updateCurrentUser.effect.ts
+++ src/app/features/auth/store/effects/updateCurrentUser.effect.ts
@@ -1,16 +1,19 @@
 import { Injectable, inject } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { catchError, map, switchMap } from "rxjs/operators";
+import { catchError, map, switchMap, tap } from "rxjs/operators";
 import { ICurrentUser } from "src/app/shared/model/currentUser.model";
 import { updateCurrentUserAction, updateCurrentUserFailureAction, updateCurrentUserSuccessAction } from "../actions/updateCurrentUser.action";
 import { of } from "rxjs";
 import { HttpErrorResponse } from "@angular/common/http";
 import { AuthService } from "../../service/auth.service";
+import { logoutAction } from "../actions/sync.action";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class UpdateCurrentUserEffect {
-  private actions$ = inject( Actions)
-  private authService = inject( AuthService)
+  private actions$ = inject(Actions);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
   updateCurrentUser$ = createEffect(() =>
     this.actions$.pipe(
@@ -31,5 +34,16 @@ export class UpdateCurrentUserEffect {
         );
       })
     )
+  );
+
+  redirectAfterCreate$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(logoutAction),
+        tap(() => {
+          this.router.navigate(['']);
+        })
+      ),
+    { dispatch: false }
   );
 }
