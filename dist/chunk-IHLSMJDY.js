3617a1e1a259d893f0800d11fff52fe0402fdbaa
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2024-03-02 09:24:18 +0300

    fix: delete dist

--- dist/chunk-IHLSMJDY.js
+++
@@ -1,1 +0,0 @@
-import{a as c}from"./chunk-LAIOXAAA.js";import{bc as o,cc as t}from"./chunk-MAJDB3HN.js";var r=o(c.LOGIN,t()),e=o(c.LOGIN_SUCCESS,t()),p=o(c.LOGIN_FAILURE,t());export{r as a,e as b,p as c};
