3a0f70c40f767368d9e4e0abb9c8979790be69dd
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2024-03-02 09:07:03 +0300

    fix

---
+++ dist/chunk-IHLSMJDY.js
@@ -0,0 +1,1 @@
+import{a as c}from"./chunk-LAIOXAAA.js";import{bc as o,cc as t}from"./chunk-MAJDB3HN.js";var r=o(c.LOGIN,t()),e=o(c.LOGIN_SUCCESS,t()),p=o(c.LOGIN_FAILURE,t());export{r as a,e as b,p as c};
